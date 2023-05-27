f54545887a9083037870969ebe294d3ab3382811
johnnybro360 <abest82@gmail.com>
2023-05-27 21:17:27 +0800

    change url

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -11,7 +11,7 @@ let cityLocation = dom.getElementById('city-name');
 let weatherImage = dom.getElementById('weather-image');
 
 async function getKey(){
-    await fetch(`https://johnnybro360.github.io/assets/downloads/test.txt`)
+    await fetch(`../downloads/test.txt`)
         .then((res) => res.text() )
         .then((data)=> console.log(data));
 }
