8f53295204b6dc6adb2aa0349c597514e3ff39ee
johnnybro360 <abest82@gmail.com>
2023-05-27 19:42:56 +0800

    update cities.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -4,7 +4,7 @@ let cityChoice = dom.getElementById('weather-select');
 
 const apiKey = "b45de6e3810499e2e37ff5f2655af8ff";
 const unit = "metric";
-const code = "AU";
+// const code = "AU";
 let temp = dom.getElementById('temp');
 let description = dom.getElementById('description');
 let cityLocation = dom.getElementById('city-name');
@@ -15,16 +15,17 @@ async function getWeatherDetails() {
     if (!cityName) {
         cityName = 'perth'
     }
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${code}&appid=${apiKey}&units=${unit}`)
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`)
         .then(function (resp) {
             return resp.json()
         }) // Convert data to json
         .then(function (data) {
             // console.log(data);
             temp.innerHTML = `${data['main']['temp'].toFixed(1)}&deg;C`;
+            cityLocation.innerHTML = `${data['name']}, ${data['sys']['country']}`;
             description.innerHTML = `${data['weather'][0]['description']}`;
-            cityLocation.innerHTML = `${cityName.charAt(0).toUpperCase()
-            + cityName.slice(1)}, ${code}`;
+            // cityLocation.innerHTML = `${cityName.charAt(0).toUpperCase()
+            // + cityName.slice(1)}, ${code}`;
             weatherImage.src = `../../assets/img/weather/${data['weather'][0]['icon']}.svg`;
             if (data['main']['temp'] >= 30) {
                 temp.style.color = '#C3272B';
