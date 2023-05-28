e5e2fabf6dd1b151ce1727f6f99678cba52dd523
johnnybro360 <abest82@gmail.com>
2023-05-28 14:28:03 +0800

    try to fix logic error 3

--- assets/js/digital-clock.js
+++ assets/js/digital-clock.js
@@ -8,6 +8,13 @@ const months = ['January', 'February', 'March', 'April', 'May', 'Jun', 'July', '
 
 const colon = dom.getElementById('colon');
 const amPm = dom.getElementById('am-pm');
+let clockDay = dom.getElementById('day')
+let clockDate = dom.getElementById('date');
+let clockHour = dom.getElementById('hour');
+let clockMin = dom.getElementById('minute');
+const twelveHourButton = dom.getElementById('12hr')
+const twentyFourHourButton = dom.getElementById('24hr')
+
 
 function flashOff() {
     colon.style.visibility = 'hidden';
@@ -21,17 +28,15 @@ setInterval(flashOff, 500);
 
 setInterval(flashOn, 1000);
 
-let clockDay = dom.getElementById('day')
-let clockDate = dom.getElementById('date');
-let clockHour = dom.getElementById('hour');
-let clockMin = dom.getElementById('minute');
 
-let date = new Date();
-setInterval(() => {
-    date = new Date();
-}, 1000)
+
+// let date = new Date();
+// setInterval(() => {
+//     date = new Date();
+// }, 1000)
 
 setInterval(() => {
+    const date = new Date();
     const minute = date.getMinutes();
     let minuteString = minute.toString()
     if (minuteString.length === 1) {
@@ -41,17 +46,17 @@ setInterval(() => {
 }, 1000)
 
 setInterval(() => {
+    const date = new Date();
     clockDay.innerText = `${days[date.getDay()]}`;
     clockDate.innerText = `${months[date.getMonth()]} ${date.getDate()}`;
-}, 1000)
+}, 10000)
 
-const twelveHourButton = dom.getElementById('12hr')
-const twentyFourHourButton = dom.getElementById('24hr')
 
 twelveHourButton.addEventListener('click', twelveHourDisplay)
 
 function twelveHourDisplay() {
-    let hour = date.getHours() % 12 || 12;
+    const date = new Date();
+    const hour = date.getHours() % 12 || 12;
     let hourString = hour.toString()
     if (hourString.length === 1) {
         hourString = '0' + hourString;
@@ -63,13 +68,13 @@ function twelveHourDisplay() {
     } else {
         amPm.innerText = `AM`;
     }
-
 }
 
 twentyFourHourButton.addEventListener('click', twentyFourHourDisplay)
 
 function twentyFourHourDisplay() {
-    let hour = date.getHours();
+    const date = new Date();
+    const hour = date.getHours();
     let hourString = hour.toString()
     if (hourString.length === 1) {
         hourString = '0' + hourString;
@@ -78,5 +83,11 @@ function twentyFourHourDisplay() {
     amPm.innerText = '';
 }
 
-
-twentyFourHourDisplay()
+setInterval(()=>{
+    if (twentyFourHourButton.checked){
+        twentyFourHourDisplay()
+    }
+    else {
+        twelveHourDisplay()
+    }
+}, 1000)
