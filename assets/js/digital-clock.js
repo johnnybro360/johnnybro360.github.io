352b317dbab09849c2e571fddc8c107a11935709
johnnybro360 <abest82@gmail.com>
2023-05-28 13:26:18 +0800

    reduce minute interval to 300 milli seconds

--- assets/js/digital-clock.js
+++ assets/js/digital-clock.js
@@ -44,7 +44,7 @@ setInterval(() => {
         minuteString = '0' + minuteString;
     }
     clockMin.innerText = minuteString;
-}, 10000)
+}, 300)
 
 setInterval(() => {
     const date = new Date();
